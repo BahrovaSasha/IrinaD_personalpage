9d94cdd0ad5cf5d44ef04342fef0c2db2d1b6e05
BahrovaSasha <bagrovaalexandra@gmail.com>
2019-04-10 17:51:40 +0300

    hovers + popup + arrow up

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,7 @@
+
 $(document).ready(function(){
+    $(".arrow-top").hide();
+
        $(window).scroll(function() {
            if ( $(this).scrollTop() >= 100 ) {
                $('.main-header').addClass('active')
@@ -8,5 +11,25 @@ $(document).ready(function(){
                $('main-header').addClass('active');
            }
        });
+
+       $(function() {
+           $(window).scroll(function() {
+               if ($(this).scrollTop() >= 1200) {
+                   $('.arrow-top').fadeIn(1000);
+               } else {
+                   $('.arrow-top').fadeOut(800);
+               }
+           });
+           $('.arrow-top').click(function() {
+               $('body,html').animate({scrollTop: 0}, 800);
+           });
+       });
+
+
+
+
+
+
+
 });
 
